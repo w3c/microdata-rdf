4d5b9108a69dd803374179c77c9291947371e72c
Gregg Kellogg <gregg@greggkellogg.net>
2015-01-31 18:14:50 -0800

    Add negative test (with slight manifest rework) to check for recursive @itemref.

--- tests/add-test.rb
+++ tests/add-test.rb
@@ -14,7 +14,7 @@ opts = GetoptLong.new(
 
 num = comment = data = result = negative = nil
 
-section_start = %r(.* id="positive-tests")m
+section_start = %r(.* id="tests")m
 
 opts.each do |opt, arg|
   case opt
@@ -28,7 +28,7 @@ opts.each do |opt, arg|
       --result    - result file, defaults to num.ttl
     )
   when '--num' then num = arg.to_i
-  when '--negative' then section_start = %r(.* id="negative-tests")m
+  when '--negative' then negative = true
   when '--comment' then comment = arg
   when "--data" then data = arg
   when "--resuit" then result = arg
@@ -57,26 +57,30 @@ result ||= "#{num_str}.ttl"
 
 # Create test files, if they don't exist
 File.open(data, "w") unless File.exist?(data)
-File.open(result, "w") unless File.exist?(result)
+File.open(result, "w") unless File.exist?(result) || negative
 
 # Add test
-output += %(
+output += %{
 <!-- Start Test Description -->
-<div itemprop="entries"
+<div itemid="#test#{num_str}"
+     itemprop="entries"
      itemscope="true"
      itemtype="http://www.w3.org/2001/sw/DataAccess/tests/test-manifest#ManifestEntry"
      class="test-entry">
-  <link itemprop="http://www.w3.org/1999/02/22-rdf-syntax-ns#type" href="http://www.w3.org/ns/rdftest#TestMicrodataEval" />
+  <link itemprop="http://www.w3.org/1999/02/22-rdf-syntax-ns#type" href="http://www.w3.org/ns/rdftest##{negative ? 'TestMicrodataNegativeSyntax' : 'TestMicrodataEval'}" />
   <span itemprop="name">Test #{num_str}</span>:
   <span itemprop="http://www.w3.org/2000/01/rdf-schema#comment"
     >#{comment}</span>
   (
-  <a itemprop="action" href="#{data}">input</a>
-  <a itemprop="result" href="#{result}">result</a>
+  <a itemprop="action" href="#{data}">input</a>}
+
+output += %{
+  <a itemprop="result" href="#{result}">result</a>} unless negative
+output += %{
   )
 </div>
 <!-- End Test Description -->
-)
+}
 
 output += input.rest
 
